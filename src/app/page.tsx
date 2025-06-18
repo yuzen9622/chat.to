cff92b4ee23563068c8b296e180bf84d1c44ad8e
yuzen9622 <oscar48079@gmail.com>
2025-06-18 11:23:42 +0800

    feat: a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,15 @@ export default function Home() {
         height={100}
         src={"/icon.png"}
       />
+      <a
+        className="inline-block px-4 py-2 font-semibold text-white underline transition-transform shadow-md rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 hover:shadow-lg hover:scale-105 underline-offset-4"
+        href="https://github.com/yuzen9622/chat.to"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        chat.to 源代碼 贊助我星星吧 😎😎
+      </a>
+
       <p className="text-white">開發者很懶 還沒開發~~</p>
     </div>
   );
